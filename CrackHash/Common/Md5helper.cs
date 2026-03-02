0cbbaa0a05ff131e5c9d239786d0ab0148fed927
Darlitch <errorguy@mail.ru>
2026-03-02 17:06:11 +0700

    ver 0.1 Create Md5Helper and Alpabet

---
+++ CrackHash/Common/Md5helper.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Common;
+
+public static class Md5helper
+{
+    public static string Compute(string input)
+    {
+        using var md5 = MD5.Create();
+        byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+        byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+        return Convert.ToHexString(hashBytes).ToLowerInvariant();
+    }
+}
