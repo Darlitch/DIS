053bb6bdd19a836dba9f4654375738095cf046f3
Darlitch <errorguy@mail.ru>
2026-03-05 17:48:26 +0700

    ver 0.21

--- CrackHash/Worker/Services/CallbackService.cs
+++ CrackHash/Worker/Services/CallbackService.cs
@@ -1,6 +1,13 @@
-﻿namespace Worker.Services;
+﻿using System.Xml.Serialization;
+using Contract.Xml;
 
-public class CallbackService
+namespace Worker.Services;
+
+public class CallbackService(IHttpClientFactory httpClientFactory)
 {
-    
+    public async Task SendResultAsync(WorkerTaskResponse result)
+    {
+        var client = httpClientFactory.CreateClient();
+        var serializer = new XmlSerializer(typeof(WorkerTaskResponse));
+    }
 }
