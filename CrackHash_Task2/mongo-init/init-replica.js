36b2e20a689547a1e5b4f6d40b38aabee64d5e42
Darlitch <errorguy@mail.ru>
2026-04-10 15:48:56 +0700

    ver 0.2 create replica-set

---
+++ CrackHash_Task2/mongo-init/init-replica.js
@@ -0,0 +1,14 @@
+﻿try {
+    const status = rs.status();
+    print("Replica set already initialized: " + status.set);
+} catch (e) {
+    rs.initiate({
+        _id: "rs0",
+        members: [
+            { _id: 0, host: "mongo1:27017", priority: 2 },
+            { _id: 1, host: "mongo2:27017", priority: 1 },
+            { _id: 2, host: "mongo3:27017", priority: 1 }
+        ]
+    });
+    print("Replica set initialized");
+}
